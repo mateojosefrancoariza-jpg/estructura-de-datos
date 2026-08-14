459435ae685cc94fbcd44ce43cf489c84a0cc95e
mateojosefrancoariza-jpg <mateojosefrancoariza@gmail.com>
2026-08-14 14:59:48 -0500

    Create calculadora.py

---
+++ calculadora.py
@@ -0,0 +1,21 @@
+def sumar(num1,  num2):
+    total =num1 + num2
+     return total
+
+def restar(num1 , num2):
+      return num1 - num2
+
+
+def main ():
+      num1 = 12
+      num2 = 45
+
+      print(sumar(num1 , num2))
+     print (restar(num1 , num2 ))
+     print(multiplicar(num1 ,num2))
+     print (dividir( num1 , num2))
+     
+     def sumar (num1 ,num2):
+        return num1,num2
+      if _name_ =="_main_":
+         main ()
