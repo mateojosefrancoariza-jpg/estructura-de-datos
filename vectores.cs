c54d6fa392918fcc436455749bccd43a64771a02
mateojosefrancoariza-jpg <mateojosefrancoariza@gmail.com>
2026-09-18 13:15:33 -0500

    Create vectores.cs

---
+++ vectores.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numero  = new int[5]
+                 { 15, 42, 8, 9, 10 };
+               Console.WriteLine( $"Elemento en [0]: {numero[0]}");
+            
+        }   
+    }
+}
